3264a35170823a234b8f7727bd08bf051fd2a068
blackCAT14-CLOUD <hafizinovus@gmail.com>
2024-07-10 03:38:29 +0200

    refractor for sorts in dsa

--- algorithms/quicksort/quick_sort.go
+++ algorithms/quicksort/quick_sort.go
@@ -1,21 +1,21 @@
 package algorithms
 
 func QuickSort(arr []int) {
-    if len(arr) < 2 {
-        return
-    }
-    pivot := arr[0]
-    var left, right []int
-    for _, v := range arr[1:] {
-        if v <= pivot {
-            left = append(left, v)
-        } else {
-            right = append(right, v)
-        }
-    }
-    QuickSort(left)
-    QuickSort(right)
-    copy(arr, left)
-    arr[len(left)] = pivot
-    copy(arr[len(left)+1:], right)
+	if len(arr) < 2 {
+		return
+	}
+	pivot := arr[0]
+	var left, right []int
+	for _, v := range arr[1:] {
+		if v <= pivot {
+			left = append(left, v)
+		} else {
+			right = append(right, v)
+		}
+	}
+	QuickSort(left)
+	QuickSort(right)
+	copy(arr, left)
+	arr[len(left)] = pivot
+	copy(arr[len(left)+1:], right)
 }
